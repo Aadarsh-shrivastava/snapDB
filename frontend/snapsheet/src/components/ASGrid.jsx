e9611cc2f86992b673464c7ae9f92eb8ba4bb3f4
arpit-shrivastava121 <arpu.rollins@gmail.com>
2024-01-23 13:42:46 +0530

    realtime parent calculation

--- frontend/snapsheet/src/components/ASGrid.jsx
+++ frontend/snapsheet/src/components/ASGrid.jsx
@@ -2,24 +2,130 @@ import React, { useState, useEffect } from "react";
 import { useSelectionContainer } from "@air/react-drag-to-select";
 import "./ExcelSheet.css";
 import "scrollable-component";
-const ExcelSheet = ({ rowData, columnData, datatree, collection_name }) => {
+const ExcelSheet = ({
+  rowData,
+  columnData,
+  datatree,
+  collection_name,
+  drillCount,
+}) => {
   const [dataTree, setDataTree] = useState([]);
   const [data, setData] = useState([]);
   const [headings, setheadings] = useState([]);
 
   useEffect(() => {
     setDataTree(datatree);
-    // console.log(JSON.stringify(dataTree));
   }, [datatree]);
 
   useEffect(() => {
     setData(rowData);
-    // console.log(JSON.stringify(rowData));
   }, [rowData]);
 
   useEffect(() => {
     setheadings(columnData);
   }, [columnData]);
+
+  const reloadTable = (newdatatree) => {
+    console.log("updating tyree nodes", newdatatree);
+    const extractingData = async (newdatatree) => {
+      var reloadedData = [];
+      var tempTreeData = [];
+      try {
+        if (newdatatree) {
+          newdatatree.forEach((doc) => {
+            let a = extractData(doc);
+            reloadedData.push(a[0]);
+            tempTreeData.push(a[1]);
+          });
+          if (newdatatree[0].data.length > 0) {
+            var entityTotal = Array(newdatatree[0].data.length).fill(0);
+            tempTreeData.forEach((node) => {
+              const valuesArray = node.data.map((item) => item.value);
+              entityTotal = sumArrays(entityTotal, valuesArray);
+            });
+            console.log("beforr new row", tempTreeData);
+            var newrow = {};
+            const keyArrays = tempTreeData[0].data.map(
+              (item) => item.column_name
+            );
+            keyArrays.forEach((keys, index) => {
+              newrow[keys] = entityTotal[index];
+            });
+            newrow[tempTreeData[0].label] = "total";
+            reloadedData.push([newrow]);
+            console.log("reloaded data", newrow);
+            setDataTree(tempTreeData);
+          }
+        }
+      } catch {
+        console.log("fetchin...");
+      }
+      setData(reloadedData);
+    };
+
+    const extractData = (doc) => {
+      if (!doc) {
+        return null;
+      }
+      const newDataTree = JSON.parse(JSON.stringify(doc));
+
+      let finalDataObject = [];
+      const traverse = (node, currentPath) => {
+        var totalsPath = {};
+        if (node.children && node.children.length > 0) {
+          const newPath = {
+            ...currentPath,
+            [String(node.label)]: String(node.name),
+          };
+          totalsPath = { ...totalsPath, ...newPath };
+          node.children.forEach((child) => traverse(child, newPath));
+        } else {
+          const newPath = {
+            ...currentPath,
+            [String(node.label)]: String(node.name),
+          };
+
+          node.data.forEach((child) => {
+            newPath[String(child.column_name)] = parseInt(child.value);
+          });
+          finalDataObject.push(newPath);
+        }
+
+        if (node.children && node.children.length > 0) {
+          let summedUpArray = Array.from(
+            { length: node.children[0].data.length },
+            () => 0
+          );
+          node.children.forEach((child) => {
+            if (!totalsPath[String(child.label)])
+              totalsPath[String(child.label)] = "total";
+            const valuesArray = child.data.map((item) => item.value);
+            summedUpArray = sumArrays(summedUpArray, valuesArray);
+          });
+          const headingsArray = node.children[0].data.map(
+            (item) => item.column_name
+          );
+          const resultObject = {};
+
+          for (let i = 0; i < headingsArray.length; i++) {
+            totalsPath[headingsArray[i]] = parseInt(summedUpArray[i]);
+            resultObject[headingsArray[i]] = parseInt(summedUpArray[i]);
+          }
+
+          node.data = JSON.parse(JSON.stringify(node.children[0].data));
+
+          node.data.forEach(
+            (child, index) => (child.value = parseInt(summedUpArray[index]))
+          );
+
+          finalDataObject.push(totalsPath);
+        }
+      };
+      traverse(newDataTree, {});
+      return [finalDataObject, newDataTree];
+    };
+    extractingData(newdatatree);
+  };
 
   const handleCellChange = (
     sheetIndex,
@@ -28,21 +134,203 @@ const ExcelSheet = ({ rowData, columnData, datatree, collection_name }) => {
     value,
     colIndex
   ) => {
+    if (data[sheetIndex][rowIndex][columnField.field] == value) {
+      return 0;
+    }
+    const path = Object.values(data[sheetIndex][rowIndex]);
+    if (Object.values(data[sheetIndex][rowIndex]).includes("total")) {
+      console.log(
+        "itds a parent row",
+        data[sheetIndex][rowIndex][columnField.field]
+      );
+      if (data[sheetIndex][rowIndex][columnField.field] == 0)
+        handleParentRowCellChange(
+          sheetIndex,
+          path,
+          columnField,
+          colIndex,
+          value
+        );
+      else
+        handleParentRowCellChangewithoutPreviousRatio(
+          sheetIndex,
+          path,
+          columnField,
+          colIndex,
+          value
+        );
+    }
+
     const newData = [...data];
     if (colIndex == 0) {
-      console.log(colIndex, columnField);
       newData[sheetIndex].forEach((datum) => {
-        datum[columnField.field] = value;
+        datum[columnField.field] = parseInt(value);
       });
       updateRootInTree(sheetIndex, value, "cell", 0);
     } else {
       newData[sheetIndex][rowIndex][columnField] = value;
     }
-    setData(newData);
-    const path = Object.values(data[sheetIndex][rowIndex]);
+    // setData(newData);
     updateTreeNodes(sheetIndex, path, columnField, value);
+    // reloadTable(dataTree);
+  };
 
-    console.log(data);
+  const handleParentRowCellChange = (
+    sheetIndex,
+    path,
+    column_name,
+    colIndex,
+    new_value
+  ) => {
+    setDataTree((prevDataTree) => {
+      const newDataTree = JSON.parse(JSON.stringify(prevDataTree));
+      var currentIndex = colIndex - drillCount;
+
+      const handleDrillDown = (node) => {
+        const traverse = (node) => {
+          if (node.children && node.children.length > 0) {
+            let sum = 0,
+              index = 0;
+            node.children.forEach((childNode) => {
+              index += 1;
+              if (node.children.length === index)
+                childNode.data[currentIndex].value =
+                  node.data[currentIndex].value - sum;
+              else {
+                childNode.data[currentIndex].value = Math.round(
+                  (node.data[currentIndex].value *
+                    childNode.data[currentIndex - 1].value) /
+                    node.data[currentIndex - 1].value
+                );
+              }
+
+              sum += childNode.data[currentIndex].value;
+              traverse(childNode);
+            });
+          }
+        };
+        traverse(node);
+      };
+
+      const findNodeByPath = (node, path, pathIndex = 0) => {
+        // console.log(path[pathIndex], node.name);
+        if (path[pathIndex + 1] === "total" && path[pathIndex] === node.name) {
+          // console.log(node, "found node");
+          node.data.forEach((element) => {
+            if (element["column_name"] === column_name.field) {
+              element.value = new_value;
+            }
+          });
+          if (currentIndex == 0) return;
+          handleDrillDown(node);
+          return node.data;
+        }
+
+        if (
+          !node ||
+          pathIndex >= path.length ||
+          node.name !== path[pathIndex]
+        ) {
+          return null; // Node not found
+        }
+
+        if (pathIndex === path.length - 1) {
+          return node; // Node found
+        }
+
+        // Continue traversing to the next level
+        for (const child of node.children) {
+          const result = findNodeByPath(child, path, pathIndex + 1);
+          if (result) {
+            return result; // Node found in one of the children
+          }
+        }
+
+        return null; // Node not found in this branch
+      };
+
+      const result = findNodeByPath(newDataTree[sheetIndex], path, 0);
+      return newDataTree;
+    });
+  };
+
+  const handleParentRowCellChangewithoutPreviousRatio = (
+    sheetIndex,
+    path,
+    column_name,
+    colIndex,
+    new_value
+  ) => {
+    console.log("handleParentRowCellChangewithoutPreviousRatio");
+    setDataTree((prevDataTree) => {
+      const newDataTree = JSON.parse(JSON.stringify(prevDataTree));
+      var currentIndex = colIndex - drillCount;
+      const handleDrillDown = (node, prev_value) => {
+        const traverse = (node) => {
+          if (node.children && node.children.length > 0) {
+            let sum = 0,
+              index = 0;
+            node.children.forEach((childNode) => {
+              index += 1;
+              if (node.children.length === index)
+                childNode.data[currentIndex].value =
+                  node.data[currentIndex].value - sum;
+              else {
+                childNode.data[currentIndex].value = Math.round(
+                  (node.data[currentIndex].value *
+                    childNode.data[currentIndex].value) /
+                    prev_value
+                );
+              }
+
+              sum += childNode.data[currentIndex].value;
+              traverse(childNode);
+            });
+          }
+        };
+        traverse(node);
+      };
+
+      const findNodeByPath = (node, path, pathIndex = 0) => {
+        var prev_value = 0;
+        if (path[pathIndex + 1] === "total" && path[pathIndex] === node.name) {
+          // console.log(node, "found node");
+          node.data.forEach((element) => {
+            if (element["column_name"] === column_name.field) {
+              prev_value = element.value;
+              element.value = new_value;
+            }
+          });
+          handleDrillDown(node, prev_value);
+          return node.data;
+        }
+
+        if (
+          !node ||
+          pathIndex >= path.length ||
+          node.name !== path[pathIndex]
+        ) {
+          return null; // Node not found
+        }
+
+        if (pathIndex === path.length - 1) {
+          return node; // Node found
+        }
+
+        // Continue traversing to the next level
+        for (const child of node.children) {
+          const result = findNodeByPath(child, path, pathIndex + 1);
+          if (result) {
+            return result; // Node found in one of the children
+          }
+        }
+
+        return null; // Node not found in this branch
+      };
+
+      const result = findNodeByPath(newDataTree[sheetIndex], path, 0);
+      return newDataTree;
+    });
   };
 
   const updateRootInTree = (sheetIndex, new_value, updateType, oldValue) => {
@@ -76,9 +364,8 @@ const ExcelSheet = ({ rowData, columnData, datatree, collection_name }) => {
   const updateTreeNodes = (sheetIndex, path, column_name, new_value) => {
     setDataTree((prevDataTree) => {
       const newDataTree = JSON.parse(JSON.stringify(prevDataTree));
-
       const findNodeByPath = (node, path, pathIndex = 0) => {
-        console.log(path[pathIndex]);
+        // console.log(path[pathIndex]);
         if (
           node.children &&
           node.children.length === 0 &&
@@ -86,7 +373,7 @@ const ExcelSheet = ({ rowData, columnData, datatree, collection_name }) => {
         ) {
           node.data.forEach((element) => {
             if (element["column_name"] === column_name.field) {
-              element.value = new_value;
+              element.value = parseInt(new_value);
             }
           });
           return node.data;
@@ -116,6 +403,9 @@ const ExcelSheet = ({ rowData, columnData, datatree, collection_name }) => {
       };
 
       const result = findNodeByPath(newDataTree[sheetIndex], path, 0);
+      // console.log('updating tyree nodes before reloading',newDataTree)
+
+      reloadTable(newDataTree);
       return newDataTree;
     });
   };
@@ -124,7 +414,6 @@ const ExcelSheet = ({ rowData, columnData, datatree, collection_name }) => {
     if (value == "" || value == headings[columnIndex].field) return;
     const newHeadings = headings;
     if (columnIndex == 0) {
-      console.log(newHeadings);
       updateRootInTree(0, value, "heading", newHeadings[columnIndex].field);
     } else {
       replaceHeadingsInDataAndTree(headings[columnIndex].field, value);
@@ -176,6 +465,7 @@ const ExcelSheet = ({ rowData, columnData, datatree, collection_name }) => {
     });
     newData.push(newArr);
     setData([...newData]); // Update state with a new array
+    window.location.reload()
   };
 
   const changeColumnNameInDataTree = (oldColumnName, newColumnName) => {
@@ -225,7 +515,6 @@ const ExcelSheet = ({ rowData, columnData, datatree, collection_name }) => {
       const traverse = (node) => {
         if (node.children && node.children.length > 0) {
           //   if (node.data[field]) console.log("field exists", node.data);
-          console.log(node);
           node.children.forEach((child) => traverse(child));
         } else {
         }
@@ -240,7 +529,6 @@ const ExcelSheet = ({ rowData, columnData, datatree, collection_name }) => {
   };
 
   const deleteColumn = (column_name) => {
-    console.log(column_name);
     const newHeadings = [...headings];
 
     const index = newHeadings.findIndex(
@@ -338,7 +626,15 @@ const ExcelSheet = ({ rowData, columnData, datatree, collection_name }) => {
   }, [dataTree]);
 
   const deleteEntity = (sheetIndex) => {
-    console.log('sheetIndex');
+    setDataTree((prevDataTree) => {
+      return prevDataTree.filter((_, index) => index !== sheetIndex);
+    });
+
+    setData((prevData) => {
+      return prevData.filter((_, index) => index !== sheetIndex);
+    });
+    // reloadTable(dataTree);
+    window.location.reload();
   };
 
   return (
@@ -409,14 +705,7 @@ const ExcelSheet = ({ rowData, columnData, datatree, collection_name }) => {
                   </svg>
                 </button>
 
-                <button
-                  className="delete-button icon icon-tabler icon-tabler-trash"
-                  onClick={() => {
-                    calDrillDown(column.field);
-                  }}
-                >
-                  Drill
-                </button>
+                
               </div>
             ))}
           </div>
@@ -425,7 +714,7 @@ const ExcelSheet = ({ rowData, columnData, datatree, collection_name }) => {
           <div className="d-flex">
             <button
               className=".delete-entity-button  icon icon-tabler icon-tabler-trash"
-              onClick={deleteEntity(sheetIndex)}
+              onClick={() => deleteEntity(sheetIndex)}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -446,13 +735,15 @@ const ExcelSheet = ({ rowData, columnData, datatree, collection_name }) => {
                 <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
               </svg>
             </button>
-            <div key={sheetIndex} className="sheet ">
+            <div key={sheetIndex} className="w-100">
               {sheet.map((row, rowIndex) => (
                 <div key={rowIndex} className="data-row">
                   {headings.map((column, colIndex) => (
                     <div
                       key={colIndex}
-                      className="data-cell"
+                      className={`${
+                        Object.values(row).includes("total") ? "total-row" : ""
+                      } data-cell `}
                       contentEditable={column.editable}
                       suppressContentEditableWarning
                       onBlur={(e) =>
@@ -474,6 +765,7 @@ const ExcelSheet = ({ rowData, columnData, datatree, collection_name }) => {
           </div>
         ))}
       </div>
+      {/* {JSON.stringify(data)} */}
     </>
   );
 };
@@ -483,12 +775,11 @@ function sumArrays(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     throw new Error("Arrays must be of the same length");
   }
-
-  // Sum up the arrays index-wise
   const result = [];
   for (let i = 0; i < arr1.length; i++) {
-    result.push(arr1[i] + arr2[i]);
+    result.push(parseInt(arr1[i]) + parseInt(arr2[i]));
   }
   return result;
 }
+
 export default ExcelSheet;
--- frontend/snapsheet/src/components/Ksheet.jsx
+++ frontend/snapsheet/src/components/Ksheet.jsx
@@ -40,6 +40,23 @@ function NewSheet() {
             data.push(a[0]);
             tempData.push(a[1]);
           });
+
+          if (treeData[0].data.length > 0) {
+            var entityTotal = Array(treeData[0].data.length).fill(0);
+            treeData.forEach((node) => {
+              const valuesArray = node.data.map((item) => item.value);
+              entityTotal = sumArrays(entityTotal, valuesArray);
+            });
+            var newrow = {};
+            const keyArrays = treeData[0].data.map((item) => item.column_name);
+            keyArrays.forEach((keys, index) => {
+              newrow[keys] = entityTotal[index];
+            });
+            console.log(newrow);
+            newrow[treeData[0].label]='total';
+            data.push([newrow]);
+          }
+
           setRowData(data);
           setColDefs(headings);
           setTemptreeData(tempData);
@@ -50,19 +67,23 @@ function NewSheet() {
     };
     extractingData();
   }, [treeData]);
+  var cal = 0,
+    rd = [];
+  if (treeData) cal = calDrillDown(treeData[0]) + 1;
+
   return (
-    // Container with theme & dimensions
     <div>
       {treeData ? (
         <div className="ag-theme-quartz" style={{ height: 500 }}>
-          {JSON.stringify(temptreeData)}
+          {/* {JSON.stringify(rowData)} */}
           <ExcelSheet
             rowData={rowData}
             columnData={colDefs}
-            datatree={treeData}
+            datatree={temptreeData}
             collection_name={paramName}
+            drillCount={cal}
           />
-          {JSON.stringify(treeData)}
+          {/* {JSON.stringify(rowData)} */}
         </div>
       ) : (
         <p>loading...</p>
@@ -94,6 +115,24 @@ const extractHeadings = (data) => {
   return finalHeadingList;
 };
 
+const calDrillDown = (node) => {
+  if (!node) {
+    return 0; // Return 0 for nodes without children
+  }
+
+  let drilldowncount = 0;
+
+  const traverse = (node, depth) => {
+    if (node.children && node.children.length > 0) {
+      depth += 1;
+      drilldowncount = Math.max(drilldowncount, depth); // Update max depth
+      node.children.forEach((child) => traverse(child, depth));
+    }
+  };
+
+  traverse(node, 0);
+  return drilldowncount;
+};
 const extractData = (data) => {
   if (!data) {
     return null;
@@ -104,28 +143,34 @@ const extractData = (data) => {
   let finalDataObject = [];
 
   const traverse = (node, currentPath) => {
+    var totalsPath = {};
     if (node.children && node.children.length > 0) {
       const newPath = {
         ...currentPath,
         [String(node.label)]: String(node.name),
       };
+      totalsPath = { ...totalsPath, ...newPath }; // Merge newPath into totalsPath
       node.children.forEach((child) => traverse(child, newPath));
     } else {
       const newPath = {
         ...currentPath,
         [String(node.label)]: String(node.name),
       };
+
       node.data.forEach((child) => {
         newPath[String(child.column_name)] = String(child.value);
       });
       finalDataObject.push(newPath);
     }
+
     if (node.children && node.children.length > 0) {
       let summedUpArray = Array.from(
         { length: node.children[0].data.length },
         () => 0
       );
       node.children.forEach((child) => {
+        if (!totalsPath[String(child.label)])
+          totalsPath[String(child.label)] = "total";
         const valuesArray = child.data.map((item) => item.value);
         summedUpArray = sumArrays(summedUpArray, valuesArray);
       });
@@ -135,13 +180,15 @@ const extractData = (data) => {
       const resultObject = {};
 
       for (let i = 0; i < headingsArray.length; i++) {
+        totalsPath[headingsArray[i]] = summedUpArray[i];
         resultObject[headingsArray[i]] = summedUpArray[i];
       }
-      node.data = node.children[0].data;
-      node.data.map((child, index) => (child.value = summedUpArray[index]));
-      console.log(node.data, "editing tree");
 
-      finalDataObject.push(resultObject);
+      node.data = JSON.parse(JSON.stringify(node.children[0].data));
+
+      node.data.forEach((child, index) => (child.value = summedUpArray[index]));
+
+      finalDataObject.push(totalsPath);
     }
   };
   traverse(newDataTree, {});
@@ -161,5 +208,11 @@ function sumArrays(arr1, arr2) {
   }
   return result;
 }
+// const extractEntitySum=(rowData)=>{
+//   const newRowData=JSON.parse(JSON.stringify(rowData));
+
+// }
+
+const sumAllEntities = () => {};
 
 export default NewSheet;
